6b4ec2274160f6267635ee35fd7f6515d0156ccf
Luna <diegounomasuno@gmail.com>
2024-01-09 20:21:58 +0100

    Created Farm Stats and Farm Ability Character

---
+++ Assets/GAME/Scripts/AbilitySystem/Farm/FarmAbilityCharacter.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FarmAbilityCharacter : AbilityCharacter
+{
+
+    private FarmStats farmStats;
+
+    protected float currentHealth;
+
+    //iniciamos abilityCharacter y farmStats
+    protected override void InitAbilityCharacter()
+    {
+        base.InitAbilityCharacter();
+
+        //Init agent paramenters 
+        if (farmStats != null)
+        {
+            currentHealth = farmStats.health;
+        }
+
+    }
+
+    protected override void Update()
+    {
+        //si el farm esta muerto o no puede hacer habilidades no hace nada 
+        if (!canDoAbilties && currentHealth <= 0)
+        {
+            return;
+        }
+
+        //funcionalidad del update del AbilityCharacter
+        base.Update();
+    }
+
+
+    public override void TakeDamage(float damage, DamageEmiterType emiterType)
+    {
+        if (emiterType == DamageEmiterType.Player || currentHealth < 0f)
+        {
+            return;
+        }
+
+        //si el emisor del daño es el player
+        else if (emiterType == DamageEmiterType.Enemy)
+        {
+            currentHealth -= damage;
+            Debug.Log(currentHealth);
+            //if (currentHealth <= 0f)
+            //{
+            //    ResetCurrentAbility();
+            //    canDoAbilties = false;
+            //    Destroy(this.gameObject);
+            //}
+        }
+
+
+    }
+}
---
+++ Assets/GAME/Scripts/Scriptables/FarmStats.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "FarmStats", menuName = "JuliosFarm/Stats/FarmStats")]
+public class FarmStats : GenericStats
+{
+
+}
