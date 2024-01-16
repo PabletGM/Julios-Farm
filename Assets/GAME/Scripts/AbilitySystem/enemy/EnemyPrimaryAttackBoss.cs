981d9297571cb4053fbfb2096a15f6eb59552387
Pablo <123249399+PabletGM@users.noreply.github.com>
2024-01-16 16:17:33 +0100

    enemyBoss funcional

--- Assets/GAME/Scripts/AbilitySystem/enemy/EnemyPrimaryAttackBoss.cs
+++ Assets/GAME/Scripts/AbilitySystem/enemy/EnemyPrimaryAttackBoss.cs
@@ -7,32 +7,31 @@ public class EnemyPrimaryAttackBoss : EnemyPrimaryAttack
 {
     public override void StartAbility(AbilityCharacter character)
     {
-        //base.StartAbility(character);
-        //character.Animator.SetTrigger("PrimaryAttack");
+        base.StartAbility(character);
+        character.Animator.SetTrigger("PrimaryAttack");
     }
 
     public override void OnReceiveAnimationEvent(AbilityCharacter character)
     {
-        //Debug.Log("Deal Damage");
+        Vector3 rayOrigin = character.transform.position + new Vector3(0f, 0.5f, 0f);
 
-        //Vector3 rayOrigin = character.transform.position + new Vector3(0f, 0.5f, 0f);
-
-        //RaycastHit[] sphereCastHitInfo = new RaycastHit[10];
-        //Physics.SphereCastNonAlloc(rayOrigin, attackRadius, character.transform.forward, sphereCastHitInfo, attackRange, attackLayerMask);
-        //if (sphereCastHitInfo.Length > 0)
-        //{
-        //    for (int i = 0; i < sphereCastHitInfo.Length; i++)
-        //    {
-        //        if (sphereCastHitInfo[i].collider != null)
-        //        {
-        //            IDamageable damageableObject = sphereCastHitInfo[i].collider.GetComponent<IDamageable>();
-        //            if (damageableObject != null)
-        //            {
-        //                Debug.DrawRay(sphereCastHitInfo[i].collider.transform.position, Vector3.up, Color.red, 2f);
-        //                damageableObject.TakeDamage(totalDamageAmount, damageEmiterType);
-        //            }
-        //        }
-        //    }
-        //}
+        RaycastHit[] sphereCastHitInfo = new RaycastHit[10];
+        Physics.SphereCastNonAlloc(rayOrigin, attackRadius, character.transform.forward, sphereCastHitInfo, attackRange.runTimeValue, attackLayerMask);
+        if (sphereCastHitInfo.Length > 0)
+        {
+            for (int i = 0; i < sphereCastHitInfo.Length; i++)
+            {
+                if (sphereCastHitInfo[i].collider != null)
+                {
+                    IDamageable damageableObject = sphereCastHitInfo[i].collider.GetComponent<IDamageable>();
+                    if (damageableObject != null)
+                    {
+                        Debug.DrawRay(sphereCastHitInfo[i].collider.transform.position, Vector3.up, Color.red, 2f);
+                        damageableObject.TakeDamage(totalDamageAmount, damageEmiterType);
+                    }
+                }
+            }
+        }
     }
 }
+
